38bc400350e9a6d04f4b93de9e4c525eeb001c39
b821213 <madeformylove@yahoo.com.tw>
2016-08-08 03:51:42 +0800

    big5_to_utf8.py: change usage to be consistent                 with other files.
    To let it be consistent, now output file must be given
    in the second argument.
    
    Usage message is added to make it easier to use as well.

--- big5_to_utf8.py
+++ big5_to_utf8.py
@@ -1,9 +1,14 @@
 import sys
-filepath = sys.argv[1]
+if len(sys.argv) <= 2:
+    print ('Usage: python3 [this] [input_file] [output_file]')
+    sys.exit(0)
+in_file = sys.argv[1]
+out_file = sys.argv[2]
 output = ''
-with open(filepath, 'r', encoding='big5', errors='ignore') as f:
+with open(in_file, 'r', encoding='big5', errors='ignore') as f:
     for line in f:
         output += line
-print (output)
+with open(out_file, 'w') as f:
+    f.write(output)
 
 
