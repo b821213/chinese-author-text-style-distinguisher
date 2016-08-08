f78e2be5422b25b8a56b2be6eb2883f1204aea8c
b821213 <madeformylove@yahoo.com.tw>
2016-08-08 16:24:15 +0800

    filepaths.py: a sample of dic file lists.
    To keep other scripts (e.g. run_test.py) simple, this file
    contains the paths of dic files. The names of each dataset
    are also contained to easily print out the results.

---
+++ filepaths.py
@@ -0,0 +1,45 @@
+sj = ['./金庸/書劍恩仇錄/dic_sj%02d' % i for i in range(1, 21)]
+bxj = ['./金庸/碧血劍/dic_bxj%02d' % i for i in range(1, 21)]
+sd = ['./金庸/射雕英雄傳/dic_sd%02d' % i for i in range(1, 41)]
+fh = ['./金庸/雪山飛狐/dic_fh%02d' % i for i in range(1, 11)]
+gd = ['./金庸/神雕俠侶/dic_gd%02d' % i for i in range(1, 41)]
+fh2 = ['./金庸/飛狐外傳/dic_feihu%02d' % i for i in range(1, 21)]
+bm = ['./金庸/白馬嘯西風/dic_%02d' % i for i in range(1, 11)]
+yyong = ['./金庸/鴛鴦刀/dic_yyong']
+ytian = ['./金庸/倚天屠龍記/dic_ytian%02d' % i for i in range(1, 41)]
+lchen = ['./金庸/連城訣/dic_%02d' % i for i in range(1, 14)]
+tlon = ['./金庸/天龍八部/dic_tlon%02d' % i for i in range(1, 51)]
+ske = ['./金庸/俠客行/dic_ske%02d' % i for i in range(1, 22)]
+jhu = ['./金庸/笑傲江湖/dic_jhu%02d' % i for i in range(1, 41)]
+ldin = ['./金庸/鹿鼎記/dic_ldin%02d' % i for i in range(1, 51)]
+ynui = ['./金庸/越女劍/dic_ynui']
+
+csc = ['./其他/七星劍/dic_%02d' % i for i in range(1, 29)]
+jhcs = ['./其他/江湖奇俠傳/dic_%02d' % i for i in range(1, 44)]
+jdai = ['./古龍/絕代雙驕/dic_%03d' % i for i in range(1,31)]
+qjian = ['./梁羽生/七劍下天山/dic_qijian%02d' % i for i in range(1,31)]
+sww = ['./梁羽生/白髮魔女傳/dic_%02d' %i for i in range(1, 33)]
+red = ['./其他/紅樓夢/dic_%03d' %i for i in range(1,121)]
+bc = ['./其他/邊城/dic_%02d' % i for i in range(1, 22)]
+
+long_work = [sj, bxj, sd, gd, fh2, ytian, jhu, ldin]
+jinyung_all = [
+        sj, bxj, sd, fh, gd, fh2, bm, yyong, ytian,
+        lchen, tlon, ske, jhu, ldin, ynui
+]
+jinyung_all_name = [
+        'sj', 'bxj', 'sd', 'fh', 'gd', 'fh2', 'bm', 'yyong',
+        'ytian', 'lchen', 'tlon', 'ske', 'jhu', 'ldin', 'ynui'
+]
+other_martials = [
+        csc, jhcs, jdai, qjian, sww
+]
+other_martials_name = [
+        'csc', 'jhcs', 'jdai', 'qjian', 'sww'
+]
+other_types = [
+        red, bc
+]
+other_types_name = [
+        'red', 'bc'
+]
