bf59268367b5d44d8d15b27d9bbd26586ba1c86f
b821213 <madeformylove@yahoo.com.tw>
2016-09-02 20:26:42 +0800

    filepaths.py: add new files and unify name rule.
    1. Some new files are added, especially lots of works
       of Yusheng Liang, to do a deeper research about
       the relation of works of Yusheng Liang and Jinyong.
    
    2. The filenames are unified to use abbreviation of the
       pinyin of the work name. If the length of the abbreviation
       is longer than 4, only first 4 alphas are left.

--- filepaths.py
+++ filepaths.py
@@ -1,45 +1,79 @@
-sj = ['./金庸/書劍恩仇錄/dic_%02d' % i for i in range(1, 21)]
+sjec = ['./金庸/書劍恩仇錄/dic_%02d' % i for i in range(1, 21)]
 bxj = ['./金庸/碧血劍/dic_%02d' % i for i in range(1, 21)]
-sd = ['./金庸/射雕英雄傳/dic_%02d' % i for i in range(1, 41)]
-fh = ['./金庸/雪山飛狐/dic_%02d' % i for i in range(1, 11)]
-gd = ['./金庸/神雕俠侶/dic_%02d' % i for i in range(1, 41)]
-fh2 = ['./金庸/飛狐外傳/dic_%02d' % i for i in range(1, 21)]
-bm = ['./金庸/白馬嘯西風/dic_%02d' % i for i in range(1, 11)]
-yyong = ['./金庸/鴛鴦刀/dic']
-ytian = ['./金庸/倚天屠龍記/dic_%02d' % i for i in range(1, 41)]
-lchen = ['./金庸/連城訣/dic_%02d' % i for i in range(1, 14)]
-tlon = ['./金庸/天龍八部/dic_%02d' % i for i in range(1, 51)]
-ske = ['./金庸/俠客行/dic_%02d' % i for i in range(1, 22)]
-jhu = ['./金庸/笑傲江湖/dic_%02d' % i for i in range(1, 41)]
-ldin = ['./金庸/鹿鼎記/dic_%02d' % i for i in range(1, 51)]
-ynui = ['./金庸/越女劍/dic']
+sdyx = ['./金庸/射雕英雄傳/dic_%02d' % i for i in range(1, 41)]
+xsfh = ['./金庸/雪山飛狐/dic_%02d' % i for i in range(1, 11)]
+sdxl = ['./金庸/神雕俠侶/dic_%02d' % i for i in range(1, 41)]
+fhwz = ['./金庸/飛狐外傳/dic_%02d' % i for i in range(1, 21)]
+bmxx = ['./金庸/白馬嘯西風/dic_%02d' % i for i in range(1, 12)]
+bmyb = ['./金庸/白馬嘯西風/原版/dic_%02d' % i for i in range(1, 10)]
+yyd = ['./金庸/鴛鴦刀/dic']
+yttl = ['./金庸/倚天屠龍記/dic_%02d' % i for i in range(1, 41)]
+lcj = ['./金庸/連城訣/dic_%02d' % i for i in range(1, 13)]
+tlbb = ['./金庸/天龍八部/dic_%02d' % i for i in range(1, 51)]
+xkx = ['./金庸/俠客行/dic_%02d' % i for i in range(1, 22)]
+xajh = ['./金庸/笑傲江湖/dic_%02d' % i for i in range(1, 41)]
+ldj = ['./金庸/鹿鼎記/dic_%02d' % i for i in range(1, 51)]
+ynj = ['./金庸/越女劍/dic']
 
-csc = ['./其他/七星劍/dic_%02d' % i for i in range(1, 29)]
-jhcs = ['./其他/江湖奇俠傳/dic_%02d' % i for i in range(1, 44)]
-jdai = ['./古龍/絕代雙驕/dic_%03d' % i for i in range(1,31)]
-qjian = ['./梁羽生/七劍下天山/dic_%02d' % i for i in range(1,31)]
-sww = ['./梁羽生/白髮魔女傳/dic_%02d' %i for i in range(1, 33)]
-red = ['./其他/紅樓夢/dic_%03d' %i for i in range(1,121)]
+qxj = ['./其他/七星劍/dic_%02d' % i for i in range(1, 29)]
+jhqx = ['./其他/江湖奇俠傳/dic_%02d' % i for i in range(1, 44)]
+jdsj = ['./古龍/絕代雙驕/dic_%03d' % i for i in range(1,127)]
+
+hlm = ['./其他/紅樓夢/dic_%03d' %i for i in range(1,121)]
 bc = ['./其他/邊城/dic_%02d' % i for i in range(1, 22)]
 
-long_work = [sj, bxj, sd, gd, fh2, ytian, jhu, ldin]
-jinyung_all = [
-        sj, bxj, sd, fh, gd, fh2, bm, yyong, ytian,
-        lchen, tlon, ske, jhu, ldin, ynui
+lhdj = ['./梁羽生/龍虎鬥京華/dic_%02d' % i for i in range(0, 13)]
+cmls = ['./梁羽生/草莽龍蛇傳/dic_%02d' % i for i in range(1, 13)]
+swqx = ['./梁羽生/塞外奇俠傳/dic_%02d' % i for i in range(1, 29)]
+qjxt = ['./梁羽生/七劍下天山/dic_%02d' % i for i in range(1,31)]
+jhsn = ['./梁羽生/江湖三女俠/dic_%02d' % i for i in range(1, 49)]
+bfmn = ['./梁羽生/白髮魔女傳/dic_%02d' % i for i in range(1, 33)]
+pzxy = ['./梁羽生/萍蹤俠影錄/dic_%02d' % i for i in range(1, 33)]
+bctn = ['./梁羽生/冰川天女傳/dic_%02d' % i for i in range(1, 41)]
+hjqq = ['./梁羽生/還劍奇情錄/dic_%02d' % i for i in range(1, 15)]
+shnx = ['./梁羽生/散花女俠/dic_%02d' % i for i in range(1, 37)]
+ndqy = ['./梁羽生/女帝奇英傳/dic_%02d' % i for i in range(1, 33)]
+kxtj = ['./梁羽生/狂俠天驕魔女/dic_%03d' % i for i in range(1, 121)]
+fyld = ['./梁羽生/風雲雷電/dic_%02d' % i for i in range(1, 67)]
+mylx = ['./梁羽生/牧野流星/dic_%02d' %i for i in range(1, 65)]
+tzjl = ['./梁羽生/彈指驚雷/dic_%02d' % i for i in range(1, 21)]
+wltj = ['./梁羽生/武林天驕/dic_%02d' % i for i in range(1, 18)]
+
+jinyong_all = [
+        sjec, bxj, sdyx, xsfh, sdxl, fhwz, bmxx, bmyb, 
+        yyd, yttl, lcj, tlbb, xkx, xajh, ldj, ynj
 ]
-jinyung_all_name = [
-        'sj', 'bxj', 'sd', 'fh', 'gd', 'fh2', 'bm', 'yyong',
-        'ytian', 'lchen', 'tlon', 'ske', 'jhu', 'ldin', 'ynui'
+jinyong_all_name = [
+        '書劍恩仇錄', '碧血劍', '射雕英雄傳', '雪山飛狐',
+        '神鵰俠侶', '飛狐外傳', '白馬嘯西風', '白馬原版',
+        '鴛鴦刀', '倚天屠龍記', '連城訣', '天龍八部',
+        '俠客行', '笑傲江湖', '鹿鼎記', '越女劍'
+]
+jinyong_long_work = [
+        sjec, bxj, sdyx, sdxl, fhwz, yttl, tlbb, xajh, ldj
+]
+yusheng_all = [
+        lhdj, cmls, swqx, qjxt,
+        jhsn, bfmn, pzxy, bctn,
+        hjqq, shnx, ndqy, kxtj,
+        fyld, mylx, tzjl, wltj
+]
+yusheng_all_name = [
+        '龍虎鬥京華', '草莽龍蛇傳', '塞外奇俠傳', '七劍下天山',
+        '江湖三女俠', '白髮魔女傳', '萍蹤俠影錄', '冰川天女傳',
+        '還劍奇情錄', '散花女俠', '女帝奇英傳', '狂俠天驕魔女',
+        '風雲雷電', '牧野流星', '彈指驚雷', '武林天驕'
 ]
 other_martials = [
-        csc, jhcs, jdai, qjian, sww
+        qxj, jhqx, jdsj
 ]
 other_martials_name = [
-        'csc', 'jhcs', 'jdai', 'qjian', 'sww'
+        '七星劍', '江湖奇俠傳', '絕代雙驕'
 ]
 other_types = [
-        red, bc
+        hlm, bc
 ]
 other_types_name = [
-        'red', 'bc'
+        '紅樓夢', '邊城'
 ]
+
